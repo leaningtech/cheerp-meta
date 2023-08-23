af32626475f06a7965ed5956d59fd40daf0e2744
Alex Bates <alex@nanaian.town>
2023-08-23 17:05:03 +0100

    maybe fix windows

---
+++ packages/cheerp-win32-x64/index.js
@@ -0,0 +1,3 @@
+const { join } = require("path");
+
+module.exports = { path: join(__dirname, "cheerp") };
--- packages/cheerp-win32-x64/index.mjs
+++
@@ -1,4 +0,0 @@
-import { URL } from "url";
-import { resolve } from "path";
-
-export const path = resolve(new URL(".", import.meta.url).pathname, "cheerp");
